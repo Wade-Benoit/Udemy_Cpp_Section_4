ef3fc026fafb475d3e4da2a77f2e2c66720d6db3
Wade_A_Benoit_Software <75183137+Wade-Benoit@users.noreply.github.com>
2021-03-17 00:20:49 -0500

    Create compiler_errors.cpp

---
+++ compiler_errors.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	 int favoriteNumber;
+     
+     
+     std::cout << "The favorite number uninitialized is " << favoriteNumber << std::endl;
+    
+    return 0;
+}
+
+
+// Unused Variable warnings when variable is declared and initialized but not used
+// Uninitiated warning when value is not set, Garbage Code
