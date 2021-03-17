b7d4d96c530d5f0ebc09314d45ecc47e443076d3
Wade_A_Benoit_Software <75183137+Wade-Benoit@users.noreply.github.com>
2021-03-17 00:28:51 -0500

    Create second_project.cpp

---
+++ second_project.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+int main()
+{
+	 std::cout << "Hello Project 2" << std::endl;
+    std::cout << "This " << "is " << "the " << "second " << "line. " << std::endl;
+    return 0;
+}
+
+//Notice the disjoint line, and how it is all inserted into the stream "cout"
