927830430c1f24b1cf720caf25a491706819558d
Wade_A_Benoit_Software <75183137+Wade-Benoit@users.noreply.github.com>
2021-03-17 00:26:54 -0500

    Create first_project.cpp

---
+++ first_project.cpp
@@ -0,0 +1,12 @@
+//Display to the terminal your own message
+//Wade B
+
+//Pre-processor directive:
+#include <iostream>
+
+int main()  {
+	 std::cout << "Hello Project 1, This is my first project for the course!" << std::endl;
+     std::cout << "This the second line of the output\n";
+     std::cout << "The namespace used here is std, or standard, and followed by a scope resolution operator ::" << std::endl;
+    return 0;
+}
