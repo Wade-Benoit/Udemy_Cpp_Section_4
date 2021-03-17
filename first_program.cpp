debbe2fa6afad48fe1f255cea7ce602867619755
Wade_A_Benoit_Software <75183137+Wade-Benoit@users.noreply.github.com>
2021-03-17 00:19:27 -0500

    Create first_program.cpp

---
+++ first_program.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main() {
+    
+    int favorite_number;
+    
+    
+    std:: cout << "Enter your favorite number between 1 - 100: " << std::endl;
+    
+    std:: cin >> favorite_number;
+    
+    std::cout << "Amazing! " << favorite_number << " is my favorite number too!" ;
+     
+    
+    
+    return 0;
+}
